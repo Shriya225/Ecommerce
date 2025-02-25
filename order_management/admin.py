0745083676de64bfb60fd635c2f962f3c07de89d
Shriya Palle <shripalle225@gmail.com>
2025-02-25 19:18:58 +0530

    -Allow admin to update Order status

--- order_management/admin.py
+++ order_management/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order,OrderItem
+from .models import Order,OrderItem,DeliveryInfo
 # Register your models here.
 
 class OrderItemInline(admin.TabularInline):
@@ -12,3 +12,4 @@ class OrderAdmin(admin.ModelAdmin):
 
 admin.site.register(Order,OrderAdmin)
 admin.site.register(OrderItem)
+admin.site.register(DeliveryInfo)
